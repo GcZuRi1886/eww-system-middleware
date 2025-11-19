82090c6b27d1b825ed7666454590bde8f1bd3e11
GcZuRi1886 <odermattfabian7@gmail.com>
2025-11-19 22:36:01 +0100

    Add flake for compilation

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Go project built with Nix flakes";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        packages.default = pkgs.buildGoModule {
+          pname = "system-info-provider";
+          version = "0.1.0";
+
+          # Path to your main.go or module root
+          src = ./.;
+
+          vendorHash = "sha256-mqui9KN6LUUghjP/sgp09QKMIxWBrtwlM6gNkA+r7lc=";
+        };
+
+        # Optional: create a dev environment with Go tools
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.go
+            pkgs.gopls
+            pkgs.go-tools
+          ];
+        };
+      }
+    );
+}
+
